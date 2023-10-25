4a2b5f204a4abfe264455844d455b37a1cd0e5c5
Paulo de Tarso <potassio@hotmail.com>
2023-10-25 20:23:05 -0300

    Primeiro botão que muda o estado de uma variável e o título

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public todos: any[] = [];
-  public title: string = 'Minhas tarefas';
+  public title1: string = 'Minhas tarefas';
+  public title2: string = 'Minhas tarefas alteradas';
+  public title: string = this.title1;
+  public alterTitle: boolean = false;
 
   constructor() {
     this.todos.push('Passear com o cachorro');
     this.todos.push('Ir ao supermercado');
     this.todos.push('Cortar o cabelo');
+  }
+
+  alterarTitle(): void {
+    if (this.alterTitle == false) {
+      this.alterTitle = true;
+    }
+    else
+      this.alterTitle = false;
+
+    if (this.alterTitle == true) {
+      this.title = this.title2;
+      return;
+    }
+
+    this.title = this.title1;
   }
 }
